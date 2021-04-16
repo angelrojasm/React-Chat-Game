616be4adf8767ae5626f22c98a468cd1bdc360e7
angelrojasicq <arojas@icq247.com>
2021-04-16 12:11:41 -0400

    Added tailwind.css configuration files into client workspace

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import "./index.css";
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
