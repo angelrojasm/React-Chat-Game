7b550041ae70cf970c7f18ff90a57fb74f508028
angelrojasicq <arojas@icq247.com>
2021-04-20 10:22:37 -0400

    Added message sending functionality

--- packages/client/src/pages/Chatroom.tsx
+++ packages/client/src/pages/Chatroom.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import socketIOClient from "socket.io-client";
+import ChatMessage from "../components/ChatMessage";
 import UserList from "../components/UserList";
 
 type AppProps = {
@@ -45,13 +46,13 @@ const Chatroom = ({ location }: AppProps): JSX.Element => {
       <div className="bg-white w-3/5 h-3/4 border-gray-300 border rounded my-8 relative shadow m-0 p-0">
         <div className="border-gray-400 border-r h-full absolute bottom-0 left-0 w-1/5 m-0 p-0">
           {/** Insert UserList Component here */}
-          {<UserList data={userList} />}
+          <UserList data={userList} />
         </div>
         <div className="absolute top-0 right-0 w-4/5 h-5/6 ">
           {/** Insert ChatList Component here */}
         </div>
-        <div className="absolute bottom-0 right-0 w-4/5 h-1/6 border-t border-gray-400 ">
-          {/** Insert ChatMessage Component here */}
+        <div className="absolute bottom-0 right-0 w-4/5 h-1/6 border-t border-gray-400 flex items-center">
+          <ChatMessage socket={socket} />
         </div>
       </div>
     </div>
