a7a62b39b4289598297eea3037b3cecef5747b8e
angelrojasicq <arojas@icq247.com>
2021-04-20 15:31:40 -0400

    Added socket rooms for broadcasting user messages and user connections/disconnections

--- packages/server/app.js
+++ packages/server/app.js
@@ -14,34 +14,25 @@ var messageList = [];
 io.on("connection", (socket) => {
   console.log("New client connected");
   socket.emit("username-request");
+  socket.join("game-room");
 
   socket.on("username-response", (response) => {
     userList[socket.handshake.issued] = response;
-    socket.emit("user-list", userList);
+    io.to("game-room").emit("user-list", userList);
   });
   socket.on("disconnect", () => {
     delete userList[socket.handshake.issued];
     console.log("Client disconnected");
+    io.to("game-room").emit("user-list", userList);
     if (Object.keys(userList).length === 0) {
       messageList.length = 0;
     }
   });
 
-  socket.on("button-message", () => {
-    console.log("recieved");
-    socket.emit("alert-message", "hello from server");
-  });
-
   socket.on("new-message", (message) => {
     messageList.push([userList[socket.handshake.issued], message]);
-    socket.emit("message-list", messageList);
+    io.to("game-room").emit("message-list", messageList);
   });
 });
-
-const getDate = (socket) => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("date", response);
-};
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
